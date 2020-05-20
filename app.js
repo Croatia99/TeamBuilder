83c2809990c09c6e037953affd56f128109701c7
Croatia99 <cyrussingularity@gmail.com>
2020-05-20 15:24:51 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+const teamArr = []
+//inqruier function to ask some q and make a new Manager;
+//go into a menu that asks if user wants to add Intern or Engineer or be done and generate team.
+//intern prompt or engineer prompt asks relevant questions and create new Intern or or Engineer, then go back to menu;
+//when done, send teamArr to the premade render function that will return the html to be writtn with fs module
+// Write code to use inquirer to gather information about the development team members,
+
+// and to create objects for each team member (using the correct classes as blueprints!)
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HELLO WORLD</h1>
+</body>
+</html>`
+
+fs.writeFile(outputPath, html, (err)=>console.log(err||"sucess"))
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
